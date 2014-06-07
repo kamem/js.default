007d722da2d641714b3cf853b750ad7370b487af
kamem <develo0@gmail.com>
2014-06-07 15:49:25 +0900

    add js.default

---
+++ js/import.js
@@ -0,0 +1,10 @@
+(function(){
+	var s = document.getElementsByTagName("script");
+	var d = s[s.length-1].src.substring(0, s[s.length-1].src.lastIndexOf("/")+1);
+	for(var i=0; i<arguments.length; i++){
+		document.write('<script type="text/javascript" src="'+d+arguments[i]+'"></script>');
+	}
+})(
+"jquery.js",
+"common.js"
+);
